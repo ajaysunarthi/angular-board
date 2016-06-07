a8a1ffbfdfeb3e90332c311c8d01396e3943a666
ajay <ajaysunarthi@iitj.ac.in>
2016-06-07 10:04:16 +0530

    angular and bootstrap

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,20 @@
-angular.module('app', []);
+angular.module('app', [])
+.controller('mainCtrl',mainCtrl);
+
+function mainCtrl () {
+	 var vm = this;
+
+	 vm.notes = [];
+
+	 vm.createNote = function () {
+	 	 var note = {
+	 	 	id: new Date().getTime(), // just a quickfix donot work in long run
+	 	 	title : 'New Note',
+	 	 	body: 'pending'
+	 	 }
+
+	 	 vm.notes.push(note);
+	 }
+
+	 
+}
